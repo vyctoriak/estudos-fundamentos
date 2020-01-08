a837841e367f78486c5a84f15cadf0b460de7505
Vyctoria <vyctoriak@gmail.com>
2020-01-08 19:42:03 -0300

    removendo comentário

--- script-predio.js
+++ script-predio.js
@@ -1,24 +1,3 @@
-// function primeiro() {
-//    const andar = window.document.getElementById('numeroAndar');
-//    console.log(andar);
-//    andar.innerText = 'Primeiro andar';
-// }
-
-// function segundo() {
-//     const andar = window.document.getElementById('numeroAndar');
-//     andar.innerText = 'Segundo andar';
-// }
-
-// function terceiro() {
-//     const andar = window.document.getElementById('numeroAndar');
-//     andar.innerText = 'Terceiro andar';
-// }
-
-// function quarto() {
-//     const andar = window.document.getElementById('numeroAndar');
-//     andar.innerText = 'Quarto andar';
-// }
-
 function mudarAndar(value, number, color) {
     const andar = window.document.getElementById('nomeAndar');
     andar.innerText = value;
