3c7a2534167a2687500ca0c1f45e7716c3a6c770
Vyctoria <vyctoriak@gmail.com>
2020-01-08 19:40:58 -0300

    adicionando número e cor ao mesmo

--- script-predio.js
+++ script-predio.js
@@ -1,20 +1,28 @@
-function primeiro() {
-   const andar = window.document.getElementById('numeroAndar');
-   console.log(andar);
-   andar.innerText = 'Primeiro andar';
-}
+// function primeiro() {
+//    const andar = window.document.getElementById('numeroAndar');
+//    console.log(andar);
+//    andar.innerText = 'Primeiro andar';
+// }
 
-function segundo() {
-    const andar = window.document.getElementById('numeroAndar');
-    andar.innerText = 'Segundo andar';
-}
+// function segundo() {
+//     const andar = window.document.getElementById('numeroAndar');
+//     andar.innerText = 'Segundo andar';
+// }
 
-function terceiro() {
-    const andar = window.document.getElementById('numeroAndar');
-    andar.innerText = 'Terceiro andar';
-}
+// function terceiro() {
+//     const andar = window.document.getElementById('numeroAndar');
+//     andar.innerText = 'Terceiro andar';
+// }
 
-function quarto() {
-    const andar = window.document.getElementById('numeroAndar');
-    andar.innerText = 'Quarto andar';
+// function quarto() {
+//     const andar = window.document.getElementById('numeroAndar');
+//     andar.innerText = 'Quarto andar';
+// }
+
+function mudarAndar(value, number, color) {
+    const andar = window.document.getElementById('nomeAndar');
+    andar.innerText = value;
+    const numero = window.document.getElementById('numeroAndar');
+    numero.innerText = number;
+    numero.style.color = color;
 }
