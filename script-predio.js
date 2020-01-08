49b3506ef1e52cbb64187a9878418348f8ab7001
Vyctoria <vyctoriak@gmail.com>
2020-01-08 18:57:25 -0300

    aparecendo status do andar ao clicar

---
+++ script-predio.js
@@ -0,0 +1,20 @@
+function primeiro() {
+   const andar = window.document.getElementById('numeroAndar');
+   console.log(andar);
+   andar.innerText = 'Primeiro andar';
+}
+
+function segundo() {
+    const andar = window.document.getElementById('numeroAndar');
+    andar.innerText = 'Segundo andar';
+}
+
+function terceiro() {
+    const andar = window.document.getElementById('numeroAndar');
+    andar.innerText = 'Terceiro andar';
+}
+
+function quarto() {
+    const andar = window.document.getElementById('numeroAndar');
+    andar.innerText = 'Quarto andar';
+}
