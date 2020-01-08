619567053a53ab87c56d6475a269a6c1977696c5
Vyctoria <vyctoriak@gmail.com>
2020-01-08 17:12:24 -0300

    redirecionando com javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+function paginaUm() {
+    window.location.href = "pag1.html";
+}
+
+function paginaDois() {
+    window.location.href = "pag2.html";
+}
+
+function paginaTres() {
+    window.location.href = "pag3.html";
+}
