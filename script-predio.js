779c6914d188e0be1f4a1b35d3344246e219375f
Vyctoria <vyctoriak@gmail.com>
2020-01-08 20:14:12 -0300

    adicionando box e mudando conforme cliques

--- script-predio.js
+++ script-predio.js
@@ -1,7 +1,13 @@
-function mudarAndar(value, number, color) {
+function mudarAndar(value, number, color, classe) {
     const andar = window.document.getElementById('nomeAndar');
     andar.innerText = value;
+
     const numero = window.document.getElementById('numeroAndar');
     numero.innerText = number;
     numero.style.color = color;
+
+    var mudarClasse = document.getElementById("miniBox");
+    mudarClasse.classList.add(classe);
+
 }
+
