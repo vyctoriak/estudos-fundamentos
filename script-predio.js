d2cc77c41562719a72a9387b2c8d0e0868cc426e
Vyctoria <vyctoriak@gmail.com>
2020-01-09 15:08:25 -0300

    organizando as funções

--- script-predio.js
+++ script-predio.js
@@ -1,13 +1,26 @@
 function mudarAndar(value, number, color, classe) {
-    const andar = window.document.getElementById('nomeAndar');
-    andar.innerText = value;
 
-    const numero = window.document.getElementById('numeroAndar');
-    numero.innerText = number;
-    numero.style.color = color;
-
-    var mudarClasse = document.getElementById("miniBox");
-    mudarClasse.className = classe;
+    trocaNomeAndar(value);
+    trocaClasse(classe, "miniBox");
+    trocaNumeroECor(number, color);
 
 }
 
+// Função que muda a classe
+function trocaClasse(classe, id) {
+    var mudarClasse = document.getElementById(id);
+    mudarClasse.className = classe;
+}
+
+// Função que muda o número do andar e a cor 
+function trocaNumeroECor(number, color) {
+    const numero = window.document.getElementById('numeroAndar');
+    numero.innerText = number;
+    numero.style.color = color;
+}
+
+// Função que muda o nome do andar 
+function trocaNomeAndar(value) {
+    const andar = window.document.getElementById('nomeAndar');
+    andar.innerText = value;
+}
