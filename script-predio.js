c13fd252b610966939aecc2d1feb6a753518c6bf
Vyctoria <vyctoriak@gmail.com>
2020-01-10 14:35:28 -0300

    time da animação concluída no javascript

--- script-predio.js
+++ script-predio.js
@@ -1,16 +1,28 @@
 // Função que abre e fecha as portas quando carrega a aplicação
 // window.onload(abreFechaPortas());
 
-// function abreFecha() {
-//     abrePortas();
-//     fechaPortas();
-// }
+// Função do onclick
+function mudarAndar(value, number, color, classe) {
+    var pessoas = document.getElementById('nomePessoa');
+    if (pessoas.value) {
+        // abrePortas();
+        // fechaPortas();
+        // trocaClasse(classe, "miniBox");
+        // trocaNomeAndar(value);
+        // trocaNumeroECor(number, color);
+        // incluirPassageiro(number)
+    } else {
+        verificaPassageiro();
+    }
+}
+
 
 //Animação pra abrir e fechar portas
 function abrePortas() {
     setTimeout(function fecharPortas() {
         trocaClasse('open-left', 'closeDoor1');
         trocaClasse('open-rigth', 'closeDoor2');
+        console.log('abrindo portas');
     }, 1000)
 }
 
@@ -18,30 +30,17 @@ function fechaPortas() {
     setTimeout(function () {
         trocaClasse('door', 'closeDoor1');
         trocaClasse('door', 'closeDoor2');
+        console.log('fechando portas');
     }, 2000)
-}
-
-// Função do onclick
-function mudarAndar(value, number, color, classe) {
-    var pessoas = document.getElementById('nomePessoa');
-    if (pessoas.value) {
-        abrePortas();
-        fechaPortas();
-        trocaNomeAndar(value);
-        trocaClasse(classe, "miniBox");
-        trocaNumeroECor(number, color);
-        incluirPassageiro(number)
-    } else {
-        verificaPassageiro();
-    }
 }
 
 // Função que muda a classe, faz o elevador subir e descer
 function trocaClasse(classe, id) {
-    setInterval(function changeClass() {
+    setTimeout(function changeClass() {
         var mudarClasse = document.getElementById(id);
         mudarClasse.className = classe;
-    }, 2000)
+        console.log('subindo elevador')
+    }, 3000)
 }
 
 // Função que muda o número do andar e a cor 
@@ -77,6 +76,37 @@ function incluirPassageiro(number) {
     }
 }
 
+function fluxoLog(value, number, color, classe) {
+    var pessoas = document.getElementById('nomePessoa');
+    if(pessoas.value) {
+        abrePortas();
+        fechaPortas();
+        console.log('abrir e fechar portas');
+    } else {
+        verificaPassageiro();
+    }
+    setTimeout(function(){
+        trocaClasse(classe, "miniBox");
+        console.log('está subindo e parou');
+    }, 3000)
+    trocaNomeAndar(value);
+    trocaNumeroECor(number, color);
+    incluirPassageiro(number);
+    setTimeout(function(){
+        abrePortas();
+        fechaPortas();
+        console.log('abrir e fechar portas de novo');
+    },6000)
+
+
+    // console.log('abrir e fechar portas');
+    // setTimeout(function(){
+    //     console.log('Está subindo e parou')
+    // }, 4000)
+    // setTimeout(function(){
+    //     console.log('abrir e fechar porta de novo')
+    // }, 6000)
+}
 
 // Validação se as portas estão abertas
 function validarPortas() {
